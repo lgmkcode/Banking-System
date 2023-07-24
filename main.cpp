1ddc4e3f918064a50f732b6a55b76c05cacd0764
lgmk.code <halileee601@gmail.com>
2023-07-24 22:37:53 +0300

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include "bank.h"
+using namespace std;
+
+int main(){
+
+account deneme1;
+deneme1.open_acc();
+int a = 0;
+
+while(a != 5){
+
+    cout << "=====================================" << endl;
+    cout << "1.scan account" << endl;
+    cout << "2.deposit" << endl << "3.withdraw" << endl;
+    cout << "4.balance" << endl << "5.exit" << endl;
+    cout << "=====================================" << endl;
+
+    cin >> a;
+/*
+    if(a == 1){
+        deneme1.open_acc();
+    }
+*/
+    if(a == 1){
+        deneme1.scan_acc();
+    }
+    else if(a == 2){
+        deneme1.get_deposit();
+    }
+    else if(a == 3){
+        deneme1.get_withdraw();
+    }
+    else if(a == 4){
+        deneme1.get_balance();
+    }
+    else if(a == 5){
+         break;
+    }
+    else{
+         cout << "incorrect entry!" << endl;
+    }
+}
+
+
+return 0;
+}
