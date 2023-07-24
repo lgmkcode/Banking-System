6cbab0e14a47b262f93356091cdf29094a991781
lgmk.code <halileee601@gmail.com>
2023-07-24 23:04:28 +0300

    change naming

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main(){
 
 account account1;
 account1.open_acc();
-int a = 0;
+int aim = 0;
 
-while(a != 5){
+while(aim != 5){
 
     cout << "=====================================" << endl;
     cout << "1.scan account" << endl;
@@ -17,32 +17,31 @@ while(a != 5){
     cout << "4.balance" << endl << "5.exit" << endl;
     cout << "=====================================" << endl;
 
-    cin >> a;
+    cin >> aim;
 /*
     if(a == 1){
         deneme1.open_acc();
     }
 */
-    if(a == 1){
+    if(aim == 1){
         account1.scan_acc();
     }
-    else if(a == 2){
+    else if(aim == 2){
         account1.get_deposit();
     }
-    else if(a == 3){
+    else if(aim == 3){
         account1.get_withdraw();
     }
-    else if(a == 4){
+    else if(aim == 4){
         account1.get_balance();
     }
-    else if(a == 5){
+    else if(aim == 5){
          break;
     }
     else{
          cout << "incorrect entry!" << endl;
     }
 }
-
 
 return 0;
 }
